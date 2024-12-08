dd678b9904eaa7f90806963dff332a93f21729c6
Furkan <esquetta@gmail.com>
2024-12-08 22:51:40 +0300

    Types defined

--- server/src/index.js
+++ server/src/index.js
@@ -1,4 +1,44 @@
-// Schema
-// SDL Scheme Defination Langueage
+import {
+    gql
+} from 'apollo-server'
 
-// string,int,ID,[]
+
+const typeDef = gql ` 
+  
+  type Query {
+    notes:[Note!]!
+
+   }
+
+ type Author{
+  id:ID!
+  name:String!
+  age:Int!
+  photo:String
+ }
+
+ type Note{  
+ id:ID!
+ title:String!
+ content:String!
+ author:Author!,
+ NoteCardBackGroundColor:Color
+ }
+
+ enum Color{
+    Red,
+    Green,
+    Blue,
+    Yellow,
+    Purple,
+    Orange,
+    Pink,
+    Black,
+    White,
+    Grey,
+    Brown,
+    Turquoise
+ }
+`
+
+module.exports = typeDef;
