211773189bb443f60244cc17c41b4020f6c473d7
Furkan <esquetta@gmail.com>
2024-12-10 19:49:18 +0300

    Resolvers added.

--- server/src/index.js
+++ server/src/index.js
@@ -1,13 +1,18 @@
-const {ApolloServer} = require('apollo-server');
+const { ApolloServer } = require("apollo-server");
 
 const { typeDefs, mocks } = require("./schema");
+const resolvers = require("./resolver");
+const NoteAPI = require("./datasources/noteapi");
 
-const  server=new ApolloServer({
-    typeDefs,
-    mocks:mocks
+const server = new ApolloServer({
+  typeDefs,
+  mocks:mocks
+//   resolvers: resolvers,
+//   dataSources: () => ({ noteapi: new NoteAPI() }),
 });
 
-
-server.listen().then(()=>{
-    console.log('server is running on port 4000. Query at : https://studio.apollographql.com/dev');
+server.listen().then(() => {
+  console.log(
+    "server is running on port 4000. Query at : https://studio.apollographql.com/dev"
+  );
 });
