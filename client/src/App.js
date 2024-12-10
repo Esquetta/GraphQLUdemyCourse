bead6157ae0f77ca5283d1b2b6f250e70d47963d
Furkan <esquetta@gmail.com>
2024-12-10 19:49:37 +0300

    Routes added.

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,21 @@
 import React from "react";
 import Layout from "./components/Layout";
 import NoteList from "./components/NoteList";
-
+import NoteDetails from "./components/NoteDetails";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <Layout>
-      <NoteList />
+      <Router>
+        <Routes>
+          {/* Define Route for NoteList */}
+          <Route exact path="/" element={<NoteList />} />
+          
+          {/* Define Route for NoteDetails */}
+          <Route path="/notes/:noteId" element={<NoteDetails />} />
+        </Routes>
+      </Router>
     </Layout>
   );
 };
