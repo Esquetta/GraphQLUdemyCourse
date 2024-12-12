742e4d447841e0f17a28de2e9da063a30ca19b05
Furkan <esquetta@gmail.com>
2024-12-12 20:48:15 +0300

    Note detail added.

--- client/src/components/NoteCard.js
+++ client/src/components/NoteCard.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const NoteCard = ({ note }) => {
   if (!note) {
@@ -9,19 +10,20 @@ const NoteCard = ({ note }) => {
     );
   }
 
-  const { title, content,author } = note;
+  const { id, title, content, author } = note;
 
   return (
-    <div className="card m-2 cursor-pointer border border-purple-400 rounded-lg hover:shadow-md hover:border-opacity-0 transform hover:-translate-y-1 transition-all duration-200">
-      <div className="m-4">
-        <h2 className="text-lg mb-2 text-purple-900">{title}</h2>
-        <p className="font-light font-mono text-sm text-indigo-600 hover:text-gray-900 transition-all duration-200">
-          {content}
-        </p>
+    <Link to={`/notes/${id}`}>
+      <div className="card m-2 cursor-pointer border border-purple-400 rounded-lg hover:shadow-md hover:border-opacity-0 transform hover:-translate-y-1 transition-all duration-200">
+        <div className="m-4">
+          <h2 className="text-lg mb-2 text-purple-900">{title}</h2>
+          <p className="font-light font-mono text-sm text-indigo-600 hover:text-gray-900 transition-all duration-200">
+            {content}
+          </p>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
 export default NoteCard;
-
