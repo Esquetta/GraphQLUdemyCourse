d4f1bbe99bdd71ba02b0f30a91dc629be38151ed
Furkan <esquetta@gmail.com>
2024-12-12 20:47:52 +0300

    Note detail added.

--- client/src/components/NoteDetails.jsx
+++ client/src/components/NoteDetails.jsx
@@ -1,12 +1,37 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import { gql, useQuery } from "@apollo/client";
+import Loading from "./Loading";
 
+export const getNote = gql`
+  query Note($noteId: ID) {
+    note(id: $noteId) {
+      id
+      title
+      content
+      NoteCardBackGroundColor
+    }
+  }
+`;
 
-const NoteDetails=()=>{
-    return(
-        <div>
-            <h1>Details</h1>
-        </div>
-    )
-}
+const NoteDetails = () => {
+  const { noteId } = useParams();
+  const { loading, error, data } = useQuery(getNote, {
+    variables: { noteId: noteId },
+  });
+  if (loading) return <Loading />;
 
-export default NoteDetails
+  if (error) return <div>Error: {error.message}</div>;
+
+  console.log(data);
+  return (
+    <div style={{backgroundColor: data.note.NoteCardBackGroundColor}}>
+        <h3>{data.note.title}</h3>
+      <p>
+        {data.note.content}
+      </p>
+    </div>
+  );
+};
+
+export default NoteDetails;
