02d70a536cc750f79c995679fdee21a582507a88
Furkan <esquetta@gmail.com>
2024-12-12 20:48:02 +0300

    Error fixing.

--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,26 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client"; // Updated import
 import "./index.css";
 import App from "./App";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
+// Apollo Client setup
 const client = new ApolloClient({
   uri: "http://localhost:4000",
   cache: new InMemoryCache(),
 });
 
-ReactDOM.render(
+// Get the root element
+const rootElement = document.getElementById("root");
+
+// Create the root
+const root = ReactDOM.createRoot(rootElement);
+
+// Render the app
+root.render(
   <React.StrictMode>
     <ApolloProvider client={client}>
       <App />
     </ApolloProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
